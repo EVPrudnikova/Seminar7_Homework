b020b2a40ef0b30739fc331df176359a0a8f2e8b
ElenaPrudnikova <elena8sen@yandex.ru>
2022-06-02 15:31:57 +0300

    add Task_47

---
+++ Task_47/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+int m = Input("m");
+int n = Input("n");
+
+double[,] array = new double[m, n];
+
+CreateArrayDouble(array);
+WriteArray(array);
+Console.WriteLine();
+
+void CreateArrayDouble(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().NextDouble() * 20 - 10;
+            array[i, j] = Math.Round(array[i, j], 1);
+        }
+    }
+}
+
+void WriteArray(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Input(string numberName)
+{
+    Console.Write($"Введите значение для {numberName}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
